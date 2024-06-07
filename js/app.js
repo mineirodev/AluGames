87124ebd9bd88a904831f96e687f72f6b69409d0
Felipe Melo <felipe.ird@gmail.com>
2024-06-06 22:33:29 -0400

    Feat: Implementação de condiçao para devolver o jogo ou nao.

--- js/app.js
+++ js/app.js
@@ -5,9 +5,12 @@ function alterarStatus(id) { //função para controle dos botões de alugar e de
     let botao = gameAlugado.querySelector('.dashboard__item__button'); //Seleciona o o elemento do botão dentro da tag li
     
     if ( botao.classList.contains('dashboard__item__button--return')) { //Condicional que verifica se o botão possui uma classe
-        botao.classList.remove('dashboard__item__button--return'); //remove a classe que modifica o botão para alugado
-        imagem.classList.remove('dashboard__item__img--rented'); //remove a classe que modifica o a imagem para alugado
-        botao.textContent = 'Alugar'; //Altera o texto do botão para Alugar
+        let opcao = confirm('Deseja devolver o jogo?');//Opçao se deseja devolver o jogo ou nao
+        if (opcao == true) { //condicional para develver o jogo
+            botao.classList.remove('dashboard__item__button--return'); //remove a classe que modifica o botão para alugado
+            imagem.classList.remove('dashboard__item__img--rented'); //remove a classe que modifica o a imagem para alugado
+            botao.textContent = 'Alugar'; //Altera o texto do botão para Alugar
+        }
     } else {
         botao.classList.add('dashboard__item__button--return'); //adiciona a classe que modifica o a imagem para alugado
         imagem.classList.add('dashboard__item__img--rented'); //adiciona a classe que modifica o a imagem para alugado
