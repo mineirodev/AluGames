dcd48eb641ebf3f31cdcec02cab0e3fe49879b36
Felipe Melo <felipe.ird@gmail.com>
2024-06-06 21:41:15 -0400

    Feat: Adiciona comentarios ao codigo

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-function alterarStatus(id) {
+function alterarStatus(id) { //função para controle dos botões de alugar e devolver
     
-    let gameAlugado = document.getElementById(`game-${id}`);
-    let imagem = gameAlugado.querySelector('.dashboard__item__img');
-    let botao = gameAlugado.querySelector('.dashboard__item__button'); 
+    let gameAlugado = document.getElementById(`game-${id}`); //Seleciona a tag li pelo ID
+    let imagem = gameAlugado.querySelector('.dashboard__item__img'); //Seleciona o o elemento de imagem dentro da tag li
+    let botao = gameAlugado.querySelector('.dashboard__item__button'); //Seleciona o o elemento do botão dentro da tag li
     
-    if ( botao.classList.contains('dashboard__item__button--return')) {
-        botao.classList.remove('dashboard__item__button--return');
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.textContent = 'Alugar';
+    if ( botao.classList.contains('dashboard__item__button--return')) { //Condicional que verifica se o botão possui uma classe
+        botao.classList.remove('dashboard__item__button--return'); //remove a classe que modifica o botão para alugado
+        imagem.classList.remove('dashboard__item__img--rented'); //remove a classe que modifica o a imagem para alugado
+        botao.textContent = 'Alugar'; //Altera o texto do botão para Alugar
     } else {
-        botao.classList.add('dashboard__item__button--return');
-        imagem.classList.add('dashboard__item__img--rented');
-        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return'); //adiciona a classe que modifica o a imagem para alugado
+        imagem.classList.add('dashboard__item__img--rented'); //adiciona a classe que modifica o a imagem para alugado
+        botao.textContent = 'Devolver'; //Altera o texto do botão para Devolver
     }
 }
